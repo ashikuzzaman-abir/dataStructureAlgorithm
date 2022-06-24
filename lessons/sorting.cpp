3d7a29dda311c032a620231248730ada0e8f652c
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2022-06-25 01:06:02 +0600

    project is up on running

---
+++ lessons/sorting.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<iomanip>
+#include<string>
+
+using namespace std;
+
+
+
+
+int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string name;
+    cin>> name;
+    cout<< name;
+    return 0;
+}
